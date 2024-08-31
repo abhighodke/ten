d2a6c7fb28cae237a8e5d4a76092bd578d4aa44e
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:35:45 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,35 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "JT", // Header Initials
-    heroTagline: "Home Sales Force Team", // Hero Tagline
-    agentName: "John Thomas",
-    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
-    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio.
+    headerInitials: "DB",
+    heroTagline: "David Black Realty",
+    agentName: "David Black",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
-With my knowledge and experience, I provide my clients with accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-Customer Service excellence is our Passion!`, // AboutMe Detailed Description
-    testimonials: [
-        { quote: "Working with John was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-        { quote: "John Thomas Realty provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-        { quote: "The dedication and expertise of John were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-        { quote: "John and his team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-        { quote: "If you’re looking for a real estate agent who goes above and beyond, John Thomas is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "john.thomas@exprealty.com", // Email
-    address: "Woodstock GA", // Address
-    phone: "770-650-9300", // Phone Number
-    footerDescription: "Home Sales Force Team is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with David Black was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "David Black provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of David Black were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "David Black and His team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, David Black is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "",
+    address: "AL, USA",
+    phone: "205-356-5412",
+    footerDescription: `David Black Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/david.r.black.39/",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "john.thomas@exprealty.com",
-        phone: "770-650-9300",
-    }, // Contact Details
+        email: "",
+        phone: "205-356-5412",
+    },
+    website: "",
+    dp: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/105016992_10220491663953736_3098027979496178616_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=g2zJnKYKt3oQ7kNvgHjMqDr&_nc_ht=scontent-lax3-1.xx&oh=00_AYBNKgKKxi4Ly6efNSqA4fFjNvryX-he_HezkI6kFta27g&oe=66F97CA4" // Agent image URL
 };
 
 export default clientData;
