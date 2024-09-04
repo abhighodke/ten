787a658c487b41ea7e6e86ae27e4076a3f756e61
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:38:02 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "DB",
-    heroTagline: "David Black Realty",
-    agentName: "David Black",
+    headerInitials: "AM",
+    heroTagline: "Austin Matthews Realty",
+    agentName: "Austin Matthews",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with David Black was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "David Black provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of David Black were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "David Black and His team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, David Black is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Austin Matthews was a game-changer for us. Him deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Austin Matthews provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Austin Matthews were evident throughout our home-buying journey. Him ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Austin Matthews and Him team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Austin Matthews is the one. Him dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "sales@theblackteam.com",
-    address: "AL, USA",
-    phone: "205-356-5412",
-    footerDescription: `David Black Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "",
+    address: "Gadsden, Alabama",
+    phone: "(256) 919-3030",
+    footerDescription: `Austin Matthews Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/david.r.black.39/",
+        facebook: "https://www.facebook.com/austingossmatthews/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "sales@theblackteam.com",
-        phone: "205-356-5412",
+        email: "",
+        phone: "(256) 919-3030",
     },
     website: "",
-    dp: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.6435-9/105016992_10220491663953736_3098027979496178616_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=g2zJnKYKt3oQ7kNvgHjMqDr&_nc_ht=scontent-lax3-1.xx&oh=00_AYBNKgKKxi4Ly6efNSqA4fFjNvryX-he_HezkI6kFta27g&oe=66F97CA4" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/458179696_10161782265282351_5437340568291328950_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=9wTcKK7QYVEQ7kNvgG0BLHi&_nc_ht=scontent-sjc3-1.xx&oh=00_AYD8KyWwlvl0NjC6rh2zLRApGcpFpC9or2EQP2JY3v2ZLg&oe=66DBB108" // Agent image URL
 };
 
 export default clientData;
